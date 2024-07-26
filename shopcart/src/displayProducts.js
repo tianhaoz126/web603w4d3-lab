972367cb311a9ef416eda1db615293af1259ee06
Eric Zhou <ericzhou1212@gmail.com>
2024-07-25 19:25:13 -0700

    Completed

--- shopcart/src/displayProducts.js
+++ shopcart/src/displayProducts.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import { Row, Col, Button } from 'reactstrap';
+import React, { useState } from 'react';
+import { Row, Col, Button, ButtonGroup } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 const DisplayProducts = ({ products, handleIncrement, handleDecrement, showProductModal }) => {
+  const [sortOption, setSortOption] = useState('normal');
+
+  const handleSortChange = (e) => {
+    setSortOption(e.target.value);
+  };
+
+  const sortedProducts = [...products].sort((a, b) => {
+    switch (sortOption) {
+      case 'lowest':
+        return a.price - b.price;
+      case 'highest':
+        return b.price - a.price;
+      default:
+        return a.id - b.id;
+    }
+  });
+
   return (
     <div>
-      {products.map(product => (
+      <h2>Products</h2>
+      <div className="sort-options">
+        <label>Sort Price By: </label>
+        <select value={sortOption} onChange={handleSortChange}>
+          <option value="normal">Normal</option>
+          <option value="lowest">Lowest</option>
+          <option value="highest">Highest</option>
+        </select>
+      </div>
+      {sortedProducts.map(product => (
         <Row key={product.id} className="mb-4 align-items-center product-row">
           <Col sm="2" className="text-center">
             <img 
@@ -18,6 +44,7 @@ const DisplayProducts = ({ products, handleIncrement, handleDecrement, showProdu
           </Col>
           <Col sm="4">
             <h5>{product.name}</h5>
+            <p>${product.price}</p>
           </Col>
           <Col sm="3" className="text-center">
             <div className="product-actions">
